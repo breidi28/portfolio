d6b9341e34ab2bc251a9477ba1613bf1a7663b4e
V.A. Bradiceanu <vladbradiceanuandrei@gmail.com>
2026-02-21 12:16:30 +0200

    Refactor RootLayout to remove BackgroundPaths component and update ThemeProvider usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { BackgroundPaths } from "@/components/ui/background-paths";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <BackgroundPaths>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
-        </BackgroundPaths>
+        </ThemeProvider>
       </body>
     </html>
   );
